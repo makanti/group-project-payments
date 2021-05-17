c6162c6a449bc2ab3a641b5da0d2b1acee70dd3e
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-05-17 13:31:46 +0100

    fixed small calc payment bug

--- src/components/CalcPayment.js
+++ src/components/CalcPayment.js
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import "./CalcPayment.css";
 
-const Payment = ({ currencies, rates, setRates }) => {
+const Payment = ({ rates }) => {
   const [selectedCurrency, setSelectedCurrency] = useState("USD");
   const [amount, setAmount] = useState("0.00");
   const [convertedValue, setConvertedValue] = useState("0.00");
 
-  const selectCurrency = (event) => {
-    const currency = event.target.value;
-    setSelectedCurrency(currency);
-  };
+  const selectCurrency = (event) => setSelectedCurrency(event.target.value);
 
-  const amountInput = (event) => {
-    setAmount(event.target.value);
-  };
+  const amountInput = (event) => setAmount(event.target.value);
 
-  const handleChange = () => {
-    setConvertedValue((amount * rates["GBP"]).toFixed(2));
-  };
-
-  useEffect(() => {
-    fetch(`https://api.frankfurter.app/latest?from=${selectedCurrency}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setRates(data.rates);
-      })
-      .catch((err) => console.error(err));
-  }, [selectedCurrency]);
+  const handleChange = () => setConvertedValue((amount / rates[selectedCurrency]).toFixed(2));
 
   return (
     <div className="CalcPayment">
       <h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
       <div className="CalcPayment-control">
         <select onChange={selectCurrency} defaultValue={selectedCurrency}>
-          {currencies.map((currency, index) => (
+          {Object.keys(rates).map((currency, index) => (
             <option key={index}>{currency}</option>
           ))}
         </select>
